20f92f6c510ceecd7f042a3c6e6d4c157e7ac115
Guilherme <108558971+guilhermetrab@users.noreply.github.com>
2022-10-11 17:11:19 -0300

    botão de menu

---
+++ script.js
@@ -0,0 +1,16 @@
+function tela(){
+    if(window.innerWidth >= 768){
+        itens.style.display = ''
+        burguer.style.display = 'none'
+    } else{
+        itens.style.display = 'none'
+        burguer.style.display = 'block'
+    }
+}
+function clickMenu(){
+    if(botoes.style.display == 'none'){
+        botoes.style.display = 'block'
+    } else{
+    botoes.style.display = 'none'
+    }
+}
